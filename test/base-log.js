0dd2a4758e6433843baac8fd0d6ff41d1aa78845
Miguel Mota <miguelmota2@gmail.com>
2015-10-23 10:02:11 -0700

    update tests

--- test/base-log.js
+++ test/base-log.js
@@ -4,7 +4,7 @@ const baseLog = require('../base-log');
 test('baseLog', function (t) {
   'use strict';
 
-  t.plan(16);
+  t.plan(19);
 
   t.notEqual(baseLog(), NaN);
   t.notEqual(baseLog(2), NaN);
@@ -13,6 +13,9 @@ test('baseLog', function (t) {
   t.notEqual(baseLog([]), NaN);
   t.notEqual(baseLog(NaN, NaN), NaN);
   t.notEqual(baseLog(1, 1), NaN);
+  t.notEqual(baseLog(-2, 8), NaN);
+  t.notEqual(baseLog(2, -8), NaN);
+  t.notEqual(baseLog(-2, -8), NaN);
   t.equal(baseLog(2, 2), 1);
   t.equal(baseLog(3, 27), 3);
   t.equal(baseLog(2, 4), 2);
